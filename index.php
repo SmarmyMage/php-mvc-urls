6584db014c57e32bb01bf331c78b3b459f6d7f07
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-03-23 12:46:00 -0500

    pretty urls

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
-require "src/controllers/products.php";
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$controller = new Products;
+$segments = explode('/', $path);
 
-$controller->index();
+// get the controller and action from the query string
+$controller = $segments[1];
+$action = $segments[2];
+
+// require the necessary controller using the variable value
+require "src/controllers/$controller.php";
+
+// assign the name of the desired controller to a $controller_object variable
+$controller_object = new $controller;
+
+// call the method from the controller using the $action value
+$controller_object->$action();
--- src/controllers/products.php
+++ src/controllers/products.php
@@ -12,4 +12,9 @@ class Products
 
         require "views/product-list.php";
     }
+
+    public function show()
+    {
+        require "views/product-show.php";
+    }
 }
