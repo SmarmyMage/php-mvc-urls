77766ba57b539936d29cf6e8a0fa5fae963e890c
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-03-25 19:08:19 -0500

    url index update

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
+// echo $path. "<br>";
 $segments = explode('/', $path);
-
+// print_r($segments);
 // get the controller and action from the query string
-$controller = $segments[1];
-$action = $segments[2];
+$controller = $segments[4]; // get the controller name from the URL for server
+$action = $segments[5]; // get the action name from the URL for server 
+// $controller = $segments[1]; // get the controller name from the URL for local
+// $action = $segments[2]; // get the action name from the URL for local
 
 // require the necessary controller using the variable value
 require "src/controllers/$controller.php";
