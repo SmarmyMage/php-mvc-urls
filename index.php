763166be8924fcce9519e99328745a7656a5641e
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-04-08 15:07:25 -0500

    mini update

--- index.php
+++ index.php
@@ -37,7 +37,6 @@ $router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => 
 $router->add("/{controller}/{id:\d+}/{action}");
 $router->add("/home/index", ["controller" => "home", "action" => "index"]);
 $router->add("/products", ["controller" => "products", "action" => "index"]);
-$router->add("/products/index", ["controller" => "products", "action" => "index"]);
 $router->add("/products/show", ["controller" => "products", "action" => "show"]);
 $router->add("/", ["controller" => "home", "action" => "index"]);
 $router->add("/{controller}/{action}");
